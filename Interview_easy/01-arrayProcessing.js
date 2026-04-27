699a6ca304cbca3cd6889c330ce119e20d84a038
Lucas <109859872+iamdevlucas123@users.noreply.github.com>
2026-04-27 09:00:09 -0300

    Update 01-arrayProcessing.js

--- Interview_easy/01-arrayProcessing.js
+++ Interview_easy/01-arrayProcessing.js
@@ -9,3 +9,24 @@ Requirements:
 - Sort in ascending order
 - Return only even numbers
 */
+
+function arrayProcess(numbers) {
+    const newArray = [...new Set(numbers)].sort().filter(i => i % 2 === 0)
+    return newArray
+}
+
+const arr = [3, 4, 1, 2, 8, 9, 2]
+console.log(arrayProcess(arr))
+
+
+
+
+
+
+/*
+    ... spread operator its make a copy of the array
+    [...new Set()] -> Sort remove all duplicates
+    .sort((a, b) => a - b) -> have a function inside that verified each number like that
+        if a - b = negative, it means b comes after a and so on
+    .filter() -> will filter each number checking if have rest 0
+*/
