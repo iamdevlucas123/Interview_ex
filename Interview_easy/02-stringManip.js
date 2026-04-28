b1ba7eba9e88e1eceade277536241f38fdbf3436
Lucas <109859872+iamdevlucas123@users.noreply.github.com>
2026-04-28 15:29:59 -0300

    Add countVowels function and example

--- Interview_easy/02-stringManip.js
+++ Interview_easy/02-stringManip.js
@@ -8,4 +8,20 @@ Requirements:
 - Count how many vowels (a, e, i, o, u) exist
 - Ignore case
 - Return the count
-*/
+*/ 
+
+function countVowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    const lowerStr = str.toLowerCase()
+    let count = 0
+
+    for (let i of lowerStr) {
+        if(vowels.includes(i))
+            count++
+    }
+
+    return count
+}
+
+let word = 'caBelo'
+console.log(countVowels(word))
