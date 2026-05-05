fde2526f5d000fb23aa2fa31ed073bd22acda884
Lucas <109859872+iamdevlucas123@users.noreply.github.com>
2026-05-05 13:35:48 -0300

    Update exercise 08

--- Interview_easy/08-fizzBuzz.js
+++ Interview_easy/08-fizzBuzz.js
@@ -12,6 +12,16 @@
  */
 
 function fizzBuzz(n) {
-    // TODO: implement
-}
+    let arr = []
 
+    for (let i = 1; i <= n; i++) {
+        let value = ""
+
+        if (i % 3 === 0) value += "Fizz"
+        if (i % 5 === 0) value += "Buzz"
+
+        arr.push(value || i)
+    }
+
+    return arr
+}
